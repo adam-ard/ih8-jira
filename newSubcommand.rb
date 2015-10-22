ce6e4387d3b5f12f56b3f6e8487c29da98fab360
Adam Ard <aard@instructure.com>
2015-10-22 16:00:39 -0600

    allow adding component with new commmand

--- newSubcommand.rb
+++ newSubcommand.rb
@@ -3,19 +3,23 @@ require File.expand_path(File.dirname(__FILE__) + '/commonSubcommand')
 $new_usage = 'Usage: ih8-jira new [options]'
 $new_argTable = [[true, 'assignee', 'a', 'new assignee'],
                  [true, 'estimate', 'e', 'new estimate'],
+                 [true, 'component', 'c', 'new component'],
                  [true, 'summary', 's', 'new summary']]
 
 def handle_new_mode(options, args)
   form_data={"fields"=> {"project"=> {"key"=> $project_key}, "issuetype"=> {"name"=> "Task"}}}
 
-  unless options['estimate'].nil?
+  if options['estimate']
     update(form_data, { 'fields'=> {'customfield_10004' => options['estimate'].to_i }})
   end
-  unless options['assignee'].nil?
+  if options['assignee']
     update(form_data, { 'fields'=> {'assignee' => {'name' => options['assignee']}}})
   end
-  unless options['summary'].nil?
+  if options['summary']
     update(form_data, { 'fields'=> {'summary' => options['summary']}})
+  end
+  if options['component']
+    update(form_data, { 'fields'=> {'components' => [{"name":options['component']}]}})
   end
 
   data,err=rest_post_request("rest/api/latest/issue/", form_data)
