2d0ea5504fda7d67fbf2eb11259e8c42760050ec
Adam Ard <adam.ard@canopytax.com>
2018-01-24 17:43:10 -0700

    fix the epic command

--- epicSubcommand.rb
+++ epicSubcommand.rb
@@ -4,15 +4,14 @@ $epic_usage = 'Usage: ih8-jira epics'
 $epic_argTable = []
 
 def print_epic_list(epic_list)
-  epic_list.select! { |e| e[2] != 'Done' }
   epic_list.each do | in_item |
-    puts "    #{format("%-7s",in_item[0])} #{format("%-.60s", in_item[1])}"
+    puts "    #{format("%-10s",in_item[0])} #{format("%-.60s", in_item[1])}"
   end
 end
 
 def handle_epic_cmd(options, args)
   jql="issuetype = Epic AND (#{$team_query})"
-  form_data={'jql' => jql, 'startAt' => 0, 'maxResults' => -1, 'fields'=> ['summary', 'customfield_10601', 'customfield_10602']}
+  form_data={'jql' => jql, 'startAt' => 0, 'maxResults' => -1, 'fields'=> ['summary']}
   data,err=rest_post_request("rest/api/latest/search/", form_data)
   if err
     return []
@@ -20,7 +19,7 @@ def handle_epic_cmd(options, args)
 
   epic_list=[]
   data['issues'].each do | item |
-    epic_list << [item['key'], item['fields']['customfield_10601'], item['fields']['customfield_10602']['value']]
+    epic_list << [item['key'], item['fields']['summary']]
   end
 
   print_epic_list(epic_list)
