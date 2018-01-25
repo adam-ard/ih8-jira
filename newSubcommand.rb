54d0a91e7c75805af63a026c998be884b96002c2
Adam Ard <adam.ard@canopytax.com>
2018-01-24 21:56:44 -0700

    add priority and description to new command

--- newSubcommand.rb
+++ newSubcommand.rb
@@ -2,24 +2,24 @@ require File.expand_path(File.dirname(__FILE__) + '/commonSubcommand')
 
 $new_usage = 'Usage: ih8-jira new [options]'
 $new_argTable = [[true, 'assignee', 'a', 'new assignee'],
-                 [true, 'estimate', 'e', 'new estimate'],
-                 [true, 'component', 'c', 'new component'],
-                 [true, 'summary', 's', 'new summary']]
+                 [true, 'summary', 's', 'new summary'],
+                 [true, 'priority', 'p', 'new priority'],
+                 [true, 'description', 'd', 'new description']]
 
 def handle_new_mode(options, args)
   form_data={"fields"=> {"project"=> {"key"=> $project_key}, "issuetype"=> {"name"=> "Task"}}}
 
-  if options['estimate']
-    update(form_data, { 'fields'=> {'customfield_10004' => options['estimate'].to_i }})
+  if options['priority']
+    update(form_data, { 'fields'=> {'priority' => {'name' => options['priority'] }}})
   end
   if options['assignee']
     update(form_data, { 'fields'=> {'assignee' => {'name' => options['assignee']}}})
   end
+  if options['description']
+    update(form_data, { 'fields'=> {'description' => options['description']}})
+  end
   if options['summary']
     update(form_data, { 'fields'=> {'summary' => options['summary']}})
-  end
-  if options['component']
-    update(form_data, { 'fields'=> {'components' => [{"name":options['component']}]}})
   end
 
   data,err=rest_post_request("rest/api/latest/issue/", form_data)
